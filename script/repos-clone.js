0d1343df6dcbbfd2e4fb88a1de62444740993bf1
Byron <byronogis@outlook.com>
2023-07-08 08:42:09 +0800

    chore!: also make parent director if not exit

--- script/repos-clone.js
+++ script/repos-clone.js
@@ -9,7 +9,7 @@ import { $, cd, chalk, fetch } from 'zx'
 export async function runReposClone(reposMap, reposDirPath) {
   try {
     await $`rm -rf ${reposDirPath}`
-    await $`mkdir ${reposDirPath}`
+    await $`mkdir -p ${reposDirPath}`
     cd(`${reposDirPath}`)
     return await Promise.all(Object.keys(reposMap).map((key) => $`git clone ${reposMap[key].ssh_url} ${key}`))
   } catch (err) {
